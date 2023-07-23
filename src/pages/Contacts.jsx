b4578abdfdeaf510e94eea98f395c180893de62c
AndriiRod <rod.andriiw@gmail.com>
2023-07-23 21:34:11 +0300

    fix

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,7 @@
 import ContactsList from 'components/ContactsList/';
 import FindField from 'components/FindField/';
-import { useGetCurrentUserQuery } from 'redux/auth';
 
 const Contacts = () => {
-  // const { data } = useGetCurrentUserQuery();
   return (
     <>
       <FindField />
